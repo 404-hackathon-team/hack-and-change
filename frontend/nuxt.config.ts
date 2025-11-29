d72c3c73454c79993612761542293b40bea5e9ca
ms0ur <maksimsuslov7142@gmail.com>
2025-11-30 01:38:29 +0300

    Api client&Auth

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -27,7 +27,7 @@ export default defineNuxtConfig({
     routeRules: {
         '/api/**': {
             proxy: {
-                to: 'http://localhost:8080/**',
+                to: 'http://localhost:8080/api/v1/**',
             },
         },
     },
